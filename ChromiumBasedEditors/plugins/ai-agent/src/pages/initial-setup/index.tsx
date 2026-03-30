88df0a12d315087d4b4a44493d598b0db5cdd589
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2026-03-30 18:35:34 +0200

    Add model config card

--- ChromiumBasedEditors/plugins/ai-agent/src/pages/initial-setup/index.tsx
+++ ChromiumBasedEditors/plugins/ai-agent/src/pages/initial-setup/index.tsx
@@ -5,6 +5,7 @@ import { FieldContainer } from "@/components/field-container";
 import { IconButton } from "@/components/icon-button";
 import { Input } from "@/components/input";
 import { Link } from "@/components/link";
+import { cn } from "@/lib/utils";
 import { provider } from "@/providers";
 
 const providersInfo = provider.getProvidersInfo();
@@ -15,9 +16,16 @@ const InitialSetup = () => {
 
   return (
     <div className="flex items-center justify-center h-full w-full bg-[var(--layout-background-color)]">
-      <div className="w-full max-w-[480px] flex flex-col">
-        <h1 className="select-none text-center text-[20px] font-bold leading-[28px] text-[var(--text-color)] mb-[16px]">
-          {t("ConnectAIProviderToEnableChat")}
+      <div
+        className={cn(
+          "w-full max-w-[560px] p-[12px_16px] flex flex-col",
+          "bg-[var(--model-config-card-background-color)]",
+          "border-[length:var(--model-config-card-border-width)] border-[color:var(--model-config-card-border-color)]",
+          "rounded-[var(--model-config-card-border-radius)]"
+        )}
+      >
+        <h1 className="select-none text-[15px] font-bold leading-[20px] text-[var(--text-color)] mb-[32px]">
+          {t("ConnectNewAIModel")}
         </h1>
 
         <div className="flex flex-col mb-[48px]">
@@ -103,7 +111,10 @@ const InitialSetup = () => {
               disableHover
             />
           </div>
-          <Button disabled={isDisabled}>{t("AddProvider")}</Button>
+          <div className="flex flex-row gap-[12px]">
+            <Button variant="default">{t("Cancel")}</Button>
+            <Button disabled={isDisabled}>{t("AddModel")}</Button>
+          </div>
         </div>
       </div>
     </div>
